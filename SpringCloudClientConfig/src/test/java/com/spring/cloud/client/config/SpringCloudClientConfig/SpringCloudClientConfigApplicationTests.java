23f5b0e2e1a6b52d011e5d62018cd5b331579c7d
郑彦闯 <yanchuang.zheng@oasismedical.cn>
2019-11-25 16:50:54 +0800

    配置客户端

---
+++ SpringCloudClientConfig/src/test/java/com/spring/cloud/client/config/SpringCloudClientConfig/SpringCloudClientConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.client.config.SpringCloudClientConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudClientConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
