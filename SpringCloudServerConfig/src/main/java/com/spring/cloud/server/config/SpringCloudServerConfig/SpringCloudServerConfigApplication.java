5ca8774500c9fc1118b84385606de3df2c0cb359
郑彦闯 <yanchuang.zheng@oasismedical.cn>
2019-11-25 16:45:51 +0800

    微服务配置服务端

---
+++ SpringCloudServerConfig/src/main/java/com/spring/cloud/server/config/SpringCloudServerConfig/SpringCloudServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.server.config.SpringCloudServerConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerConfigApplication.class, args);
+	}
+
+}
